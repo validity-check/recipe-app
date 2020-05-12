24c836d094c702f9d869508d66c9a4770956956a
validity-check <mzaiddar77@gmail.com>
2020-05-12 17:48:47 +0100

    Save data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipes.service';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService
+    ) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(
+      'https://recipes-app-a004d.firebaseio.com/recipes.json',
+      recipes
+    )
+    .subscribe(response => {
+      console.log(response);
+    });
+  }
 }
