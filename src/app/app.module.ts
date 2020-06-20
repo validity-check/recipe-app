000ca036cbcbedf59f943ce06e8d02bf2f051ed9
validity-check <mzaiddar77@gmail.com>
2020-06-20 14:57:07 +0100

    Shopping List Module & Auth Module lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -20,8 +18,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule,
   ],
