931bafbbaf77ad21861b17ec1fd7fa72ea47dd60
validity-check <mzaiddar77@gmail.com>
2020-07-25 13:07:46 +0100

    Effects

---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,42 @@
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { switchMap, catchError, map } from "rxjs/operators";
+import { environment } from "../../../environments/environment";
+
+import { HttpClient } from "@angular/common/http";
+
+import * as AuthActions from "./auth.actions";
+import { of } from "rxjs";
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+export class AuthEffects {
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" +
+          environment.firebaseAPIKey,
+        {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true,
+        }
+      );
+    }).pipe(catchError(error => {
+      of();
+    }), map(resData => {
+      of()
+    }));
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
